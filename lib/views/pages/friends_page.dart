975a593818d45521b7085518c5a85244c60c94bf
JoannVetter <joann.vetter.dev@proton.me>
2024-12-23 23:46:04 +0100

    (feat) friends: Adding a sample friends page, needs improvement

--- lib/views/pages/friends_page.dart
+++ lib/views/pages/friends_page.dart
@@ -1,4 +1,4 @@
-
+import 'package:ski_planner/views/pages/profile_page.dart';
 import 'package:ski_planner/views/widgets/navbar.dart';
 import 'package:flutter/material.dart';
 
@@ -10,15 +10,44 @@ class FriendsPage extends StatefulWidget {
 }
 class _FriendsPageState extends State<FriendsPage> {
 
+  final List<Map<String, String>> friends = [
+    {"name": "Alice", "email": "alice@example.com", "avatar": "https://i.pravatar.cc/300"},
+    {"name": "Bob", "email": "bob@example.com", "avatar": "https://i.pravatar.cc/200"},
+    {"name": "Charlie", "email": "charlie@example.com", "avatar": "https://i.pravatar.cc/150"},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: NavBar(
-        index: 1,
+      appBar: AppBar(
+        title: Text('Friends List'),
+        centerTitle: true,
       ),
-      backgroundColor: Colors.green,
-      extendBody: true,
-      extendBodyBehindAppBar: true,
+      body: ListView.builder(
+        itemCount: friends.length,
+        itemBuilder: (context, index) {
+          final friend = friends[index];
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(friend["avatar"]!),
+            ),
+            title: Text(friend["name"]!),
+            subtitle: Text(friend["email"]!),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ProfilePage(
+                    // username: friend["name"]!,
+                    // email: friend["email"]!,
+                    // avatarUrl: friend["avatar"]!,
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
