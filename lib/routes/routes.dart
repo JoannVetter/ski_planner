bf308e2f4751f65203666f48cf82fae3cbb48cb4
JoannVetter <joann.vetter.dev@proton.me>
2024-12-23 18:18:45 +0100

    (feat) routing: Adding a routing logic

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,6 +1,21 @@
-class Routes {
-  static const String initialRoute = '/';
+import 'package:flutter/material.dart';
+import 'package:ski_planner/views/profile_page.dart';
 
-  static final routes = [
-  ];
+class AppRouter {
+  static const String profile = '/profile';
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case profile:
+        return MaterialPageRoute(builder: (_) => ProfilePage());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
 }
