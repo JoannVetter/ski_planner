7b1264bc30edde4da35a068a0148bf4209bd1cdb
Ludovic Schini <ludovicschini67@gmail.com>
2024-12-23 17:26:55 +0100

    (feat) navbar: Added navbar.dart to views/widgets and used it in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ski_planner/views/widgets/navbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -56,6 +57,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String selectedPage = '';
 
   void _incrementCounter() {
     setState(() {
@@ -119,7 +121,9 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), // This trailing comma makes auto-formatting nicer for build methods
+
+      bottomNavigationBar: NavBar()
     );
-  }
+    }
 }
